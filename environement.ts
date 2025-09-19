d8e9e9b269f9a85ff6be9b247df96bb8b499bcdb
Taha Hamim Boughanmi <99847753+tahahamim86@users.noreply.github.com>
2025-09-19 23:13:40 +0100

    Update environement.ts

--- environement.ts
+++ environement.ts
@@ -1,5 +1,6 @@
 // environment.ts
 export const environment = {
   production: false,
-  apiUrl: 'http://127.0.0.1:9300/api' // or http://localhost:8080/api for local dev
+  apiUrl: 'https://backds-2-wt30.onrender.com/api' // or http://localhost:8080/api for local dev
 };
+
